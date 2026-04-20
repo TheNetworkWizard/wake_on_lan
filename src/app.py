feac04825cef4375691f63d7ab7134b63d4a58dd
Matthew Sullivan <mattsullivan2306@gmail.com>
2026-04-20 22:41:40 +0100

    Added DB to backend

--- src/app.py
+++ src/app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, abort, redirect
 import os
+import sqlite3
+import json
 app = Flask(__name__)
+
+DB = '../servers.db'
 
 server_list = {
         "zion": {
@@ -18,34 +22,91 @@ server_list = {
 # Define a route for the root URL
 @app.route("/getServerList")
 def getServerList():
+    conn = sqlite3.connect( DB )
+    conn.row_factory = sqlite3.Row # This enables column access by name: row['column_name'] 
+    db = conn.cursor()
+
+    rows = db.execute('''SELECT server_name, server_ip, status from server_list''').fetchall()
+
+    conn.commit()
+    conn.close()
+
+    #if json_str:
+    #    return json.dumps( [dict(ix) for ix in rows] ) #CREATE JSON
+
+    servers = {}
+    for row in rows:
+        servers[row['server_name']] = {}
+        servers[row['server_name']]['server_name'] = row['server_name'] 
+        servers[row['server_name']]['status'] = row['status'] 
+        servers[row['server_name']]['server)ip'] = row['server_ip'] 
+
+    return servers
+    
+    return json.dumps( [dict(ix) for ix in rows] ) 
+
+    return rows
+
     return server_list
 
 @app.route("/getServerStatus/<server_name>")
 def getServerStatus(server_name):
-    if server_name not in server_list:
+    conn = sqlite3.connect( DB )
+    conn.row_factory = sqlite3.Row # This enables column access by name: row['column_name'] 
+    db = conn.cursor()
+
+    rows = db.execute('''SELECT * from server_list where server_name = ?''', (server_name, )).fetchall()
+
+    conn.commit()
+    conn.close()
+
+    if len(rows) == 0:
         abort(404)
-    return server_list[server_name]
+    
+    return json.dumps( [dict(ix) for ix in rows][0] ) 
 
 @app.route("/startServer/<server_name>")
 def startServer(server_name):
-    if server_name not in server_list:
+    conn = sqlite3.connect( DB )
+    conn.row_factory = sqlite3.Row # This enables column access by name: row['column_name'] 
+    db = conn.cursor()
+
+    rows = db.execute('''SELECT * from server_list where server_name = ?''', (server_name, )).fetchall()
+
+    conn.commit()
+   
+
+    if len(rows) == 0:
+        conn.close()
         abort(404)
 
-    if not server_list[server_name]['status']:
-        server_list[server_name]['status'] = True
+    db.execute('''UPDATE server_list set status = 1 where server_name = ?''', (server_name,))
+    conn.commit()
+    conn.close()
 
-    return server_list[server_name]
+    return json.dumps( [dict(ix) for ix in rows][0] ) 
 
 
 @app.route("/stopServer/<server_name>")
 def stopServer(server_name):
-    if server_name not in server_list:
+    conn = sqlite3.connect( DB )
+    conn.row_factory = sqlite3.Row # This enables column access by name: row['column_name'] 
+    db = conn.cursor()
+
+    rows = db.execute('''SELECT * from server_list where server_name = ?''', (server_name, )).fetchall()
+
+    conn.commit()
+   
+
+    if len(rows) == 0:
+        conn.close()
         abort(404)
 
-    if server_list[server_name]['status']:
-        server_list[server_name]['status'] = False
+    db.execute('''UPDATE server_list set status = 0 where server_name = ?''', (server_name,))
+    conn.commit()
+    conn.close()
 
-    return server_list[server_name]
+    return json.dumps( [dict(ix) for ix in rows][0] ) 
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
